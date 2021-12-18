5ec399853e4ff095a0f8931446158c1b3a2680eb
BOTZ-DHANI <dhanigans87@gmail.com>
2021-12-18 05:50:26 -0800

    V8

---
+++ baca_!!!.js
@@ -0,0 +1,44 @@
+==========[ INFO 1 ]==========
+
+       false : untuk matiin
+       true : untuk hidupin
+
+Paham ?
+
+==========[ INFO 2 ]==========
+
+Kalau Error Dari Luh Jangan Chat Gw :(
+Kalau Gak Tau Mending Estrak Ulang
+
+File Foto :
+1. dhani.jpg , Minimal di bawah 100 Kb 
+2. dhanithumb.jpg , Minimal Di Bawah 200 Kb
+
+Paham ?
+
+
+==========[ INFO 3 ]==========
+
+🗣️ : Bang Fiturnya Kok Error Sih ?
+
+👤 : F , Itu Karna Apikey nya habis , Mending Beli Sendiri :)
+Saya Tidak Mampu Beli Apikey :)
+
+🗣️ : Ouh 
+
+🗣️ : Btw ini kok pas saya run error ? 
+
+👤 : Kalau  Gak Tau Estrak Ulang Aja Bang :)
+
+==========[ REST API ]==========
+
+lolkey : https://api.lolhuman.xyz/docs
+ 
+zekskey : https://api.zeks.me
+
+==========[ AKHIR ]==========
+
+Jangan Lupa Subscribe YT : Bot Dhani ?
+jan lupa dukung gua terus
+
+Mohon Di Baca Semua Yak !!
